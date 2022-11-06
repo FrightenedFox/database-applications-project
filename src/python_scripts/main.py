c89d2cebf1454bf7c42810cfef07df737bc8cdf7
Vitalii Morskyi <vitalij20358@gmail.com>
2022-11-06 13:29:39 +0100

    Create DB Schema

--- src/python_scripts/main.py
+++ src/python_scripts/main.py
@@ -1,6 +1,9 @@
 from logging_setup import initialize_logging
 from pull_from_usos import pull_data
+from db_utils import initialize_db
 
 if __name__ == '__main__':
     initialize_logging()
-    pull_data()
+    # pull_data()
+    initialize_db(echo=True)
+
