bc2487139e38ecd2a53262fba9b8d099396ed1f4
Norbert-Cyran <norbik601@gmail.com>
2022-12-17 13:40:46 +0100

    7/8

--- src/sql_scripts/functions.sql
+++ src/sql_scripts/functions.sql
@@ -1,88 +1,202 @@
-
 -- 1
-create or replace function sprawdzanie_wolnego_miejsca(grupa_id in unit_groups.unit_group_id%TYPE)
-returns wynik
-language plpgsql
-as $$
-declare
-ilosc_osob integer
-wynik bool
-BEGIN
-    SELECT count(user_usos_id) into ilosc_osob from users_groups
-        JOIN unit_groups ug on ug.unit_group_id = users_groups.group_id
-        JOIN activities a on ug.unit_group_id = a.unit_group
-        JOIN rooms r on a.room = r.room_id where users_groups.group_id= grupa_id;
-
-
-if ilosc_osob > rooms.capacity
-    then wynik = FALSE;
-    else
-    wynik = TRUE;
-
-end if;
-end $$;
---2
-create or replace function zajecia_w_danym_czasie(id_studenta in users.usos_id%TYPE,czas_rozpoczecia timestamp with time zone,
-czas_zakonczenia timestamp with time zone)
-returns bool
-    language plpgsql
-as $$
-BEGIN
-    CREATE TEMPORARY TABLE czas_temp(
-        start_time timestamp with time zone NOT NULL,
-        end_time timestamp with time zone NOT NULL,
-        unit_group integer);
-
-    SELECT start_time, end_time, unit_group INTO table czas_temp from activities
-    JOIN unit_groups ug on activities.unit_group = ug.unit_group_id
-    JOIN users_groups u on ug.unit_group_id = u.group_id
-    JOIN users u2 on u2.usos_id = u.user_usos_id where usos_id = id_studenta;
-for loop
-        if czas_temp.start_time = czas_rozpoczecia && czas_temp.end_time = czas_zakonczenia
-            then return FALSE
-        elsif czas_temp.start_time != czas_rozpoczecia && czas_temp.end_time != czas_zakonczenia
-            then return TRUE
-        end if;
-    end loop;
-    end $$;
-
--- 6
-create or replace procedure ilosc_godzin_wykladowcy(wykladowca in teachers.teacher_usos_id%TYPE)
-language plpgsql
-as $$
-DECLARE
-
-zajecia record;
-grupy int[];
-czas1 integer;
-czas2 integer;
-Begin
-    for grupy IN SELECT unit_group from group_teacher where teacher= wykladowca
-    loop
-    end loop;
-FOR zajecia IN SELECT start_time, end_time from activities where unit_group = grupy
-        loop
-        czas1 := age(activities.end_time, activities.start_time) AS difference;
-        czas2 := czas2+czas1;
-        end loop;
-end;
+CREATE OR REPLACE FUNCTION sprawdzanie_wolnego_miejsca(grupa_id unit_groups.unit_group_id%TYPE)
+    RETURNS wynik
+    LANGUAGE plpgsql
+AS
 $$
+DECLARE
+    ilosc_osob integer
 
+BEGIN
+    SELECT COUNT(user_usos_id)
+    INTO ilosc_osob
+    FROM users_groups
+             JOIN unit_groups ug ON ug.unit_group_id = users_groups.group_id
+             JOIN activities a ON ug.unit_group_id = a.unit_group
+             JOIN rooms r ON a.room = r.room_id
+    WHERE users_groups.group_id = grupa_id;
+
+
+    IF ilosc_osob > rooms.capacity
+    THEN
+        wynik = FALSE;
+    ELSE
+        wynik = TRUE;
+
+    END IF;
+END
+$$;
+--2
+CREATE OR REPLACE FUNCTION zajecia_studenta_w_danym_czasie(student users.usos_id%TYPE,
+                                                           czas_poczatkowy activities.start_time%TYPE,
+                                                           czas_koncowy activities.end_time%TYPE)
+    RETURNS bool
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    wynik INT;
+BEGIN
+    SELECT COUNT(activity_id)
+    INTO wynik
+    FROM activities act
+             INNER JOIN unit_groups ung ON act.unit_group = ung.unit_group_id
+             INNER JOIN users_groups usg ON ung.unit_group_id = usg.group_id
+             INNER JOIN users u ON u.usos_id = usg.user_usos_id
+    WHERE (start_time BETWEEN czas_poczatkowy AND czas_koncowy
+        OR end_time BETWEEN czas_poczatkowy AND czas_koncowy)
+      AND u.usos_id = student;
+    IF wynik > 0 THEN
+        RETURN FALSE;
+    ELSIF wynik = 0 THEN
+        RETURN TRUE;
+    END IF;
+END;
+$$
+SELECT zajecia_studenta_w_danym_czasie(234394, timestamp '2022-12-16 16:46:00', timestamp '2022-12-16 17:30:00')
+--3
+CREATE OR REPLACE FUNCTION zajecia_wykladowcy_w_danym_czasie(wykladowca teachers.teacher_usos_id%TYPE,
+                                                             czas_poczatkowy activities.start_time%TYPE,
+                                                             czas_koncowy activities.end_time%TYPE)
+    RETURNS bool
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    wynik INT;
+BEGIN
+    SELECT COUNT(activity_id)
+    INTO wynik
+    FROM activities act
+             INNER JOIN unit_groups ug ON ug.unit_group_id = act.unit_group
+             INNER JOIN group_teacher gt ON ug.unit_group_id = gt.unit_group
+             INNER JOIN teachers t ON t.teacher_usos_id = gt.teacher
+    WHERE (start_time BETWEEN czas_poczatkowy AND czas_koncowy
+        OR end_time BETWEEN czas_poczatkowy AND czas_koncowy)
+      AND t.teacher_usos_id = wykladowca;
+    IF wynik > 0 THEN
+        RETURN FALSE;
+    ELSIF wynik = 0 THEN
+        RETURN TRUE;
+    END IF;
+END;
+$$
+SELECT zajecia_wykladowcy_w_danym_czasie(8640, timestamp '2022-12-16 09:25:00', timestamp '2022-12-16 10:00:00')
+--4
+CREATE OR REPLACE FUNCTION zajecia_grupy_w_danym_czasie(grupa unit_groups.unit_group_id%TYPE,
+                                                        czas_poczatkowy activities.start_time%TYPE,
+                                                        czas_koncowy activities.end_time%TYPE)
+    RETURNS bool
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    wynik INT;
+BEGIN
+    SELECT COUNT(activity_id)
+    INTO wynik
+    FROM activities act
+             INNER JOIN unit_groups ug ON ug.unit_group_id = act.unit_group
+    WHERE (start_time BETWEEN czas_poczatkowy AND czas_koncowy
+        OR end_time BETWEEN czas_poczatkowy AND czas_koncowy)
+      AND ug.unit_group_id = grupa;
+    IF wynik > 0 THEN
+        RETURN FALSE;
+    ELSIF wynik = 0 THEN
+        RETURN TRUE;
+    END IF;
+END;
+$$
+SELECT zajecia_grupy_w_danym_czasie(23, timestamp '2022-12-16 15:25:00', timestamp '2022-12-16 16:00:00')
+--5
+CREATE OR REPLACE FUNCTION wolna_sala(sala rooms.room_id%TYPE,
+                                      czas_poczatkowy activities.start_time%TYPE,
+                                      czas_koncowy activities.end_time%TYPE)
+    RETURNS bool
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    wynik INT;
+BEGIN
+    SELECT COUNT(activity_id)
+    INTO wynik
+    FROM activities act
+             INNER JOIN rooms r ON r.room_id = act.room
+    WHERE (start_time BETWEEN czas_poczatkowy AND czas_koncowy
+        OR end_time BETWEEN czas_poczatkowy AND czas_koncowy)
+      AND r.room_id = sala;
+    IF wynik > 0 THEN
+        RETURN FALSE;
+    ELSIF wynik = 0 THEN
+        RETURN TRUE;
+    END IF;
+END;
+$$
+SELECT wolna_sala('P.15(P-2.401)', timestamp '2022-12-16 09:25:00', timestamp '2022-12-16 10:00:00')
+-- 6
+CREATE OR REPLACE FUNCTION ilosc_godzin_wykladowcy(wykladowca teachers.teacher_usos_id%TYPE) RETURNS FLOAT AS
+$$
+DECLARE
+    zajecie     RECORD;
+    grupa       INT;
+    laczny_czas FLOAT := 0.0;
+BEGIN
+    FOR grupa IN SELECT unit_group FROM group_teacher WHERE teacher = wykladowca
+        LOOP
+            RAISE NOTICE 'Grupa %', grupa;
+            FOR zajecie IN SELECT start_time, end_time FROM activities WHERE unit_group = grupa
+                LOOP
+                    laczny_czas := laczny_czas +
+                                   EXTRACT(EPOCH FROM AGE(zajecie.end_time, zajecie.start_time)) / 3600 AS difference;
+                END LOOP;
+        END LOOP;
+    RETURN laczny_czas;
+END;
+$$ LANGUAGE plpgsql;
+SELECT ilosc_godzin_wykladowcy(186812);
+--7
+CREATE OR REPLACE FUNCTION ilosc_godzin_studenta(student users.usos_id%TYPE) RETURNS FLOAT AS
+$$
+DECLARE
+    zajecie     RECORD;
+    grupa       INT;
+    laczny_czas FLOAT := 0.0;
+BEGIN
+    FOR grupa IN SELECT group_id FROM users_groups WHERE user_usos_id = student
+        LOOP
+            RAISE NOTICE 'Grupa %', grupa;
+            FOR zajecie IN SELECT start_time, end_time FROM activities WHERE unit_group = grupa
+                LOOP
+                    laczny_czas := laczny_czas +
+                                   EXTRACT(EPOCH FROM AGE(zajecie.end_time, zajecie.start_time)) / 3600 AS difference;
+                END LOOP;
+        END LOOP;
+    RETURN laczny_czas;
+END;
+$$ LANGUAGE plpgsql;
+SELECT ilosc_godzin_studenta(233921);
 
 -- 8
-create extension cube;
-create extension earthdistance;
-create or replace function odleglosc_miedzy_budynkami(budynek_nr1 text, budynek_nr2 text)
-returns float
-    language plpgsql
-as $$
-declare
-odleglosc float;
-punkt1 double precision;
-punkt2 double precision;
+CREATE OR REPLACE FUNCTION odleglosc_miedzy_budynkami(budynek_a_id buildings.building_id%TYPE,
+                                                      budynek_b_id buildings.building_id%TYPE)
+    RETURNS DOUBLE PRECISION
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    odleglosc   DOUBLE PRECISION;
+    dlugosc_a   DOUBLE PRECISION;
+    szerokosc_a DOUBLE PRECISION;
+    budynek_b   buildings%ROWTYPE;
+BEGIN
+    SELECT radians(bud.longitude), radians(bud.latitude) INTO dlugosc_a, szerokosc_a FROM buildings bud WHERE building_id = budynek_a_id;
+    SELECT bud.building_id, bud.building_name ,radians(bud.longitude), radians(bud.latitude) INTO budynek_b FROM buildings bud WHERE building_id = budynek_b_id;
+    odleglosc = 2 * 6371000 * ASIN(SQRT(SIN((budynek_b.longitude - dlugosc_a) / 2) ^ 2 +
+                                        COS(dlugosc_a) * COS(budynek_b.longitude) *
+                                       SIN((budynek_b.latitude - szerokosc_a) / 2) ^ 2));
 
-begin
-    select point(longitude,latitude) <@> point(longitude,latitude) as distance into odleglosc
-    from buildings where building_id=budynek_nr1 and building_id=budynek_nr2;
-return odleglosc
-end; $$
+    RETURN odleglosc;
+END;
+$$
+Select odleglosc_miedzy_budynkami('F','P')
